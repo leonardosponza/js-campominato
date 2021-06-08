18fb0191b47caed404ae8e0185c63333203242a8
leonardo <sponzal4@gmail.com>
2021-06-08 16:36:52 +0200

    seconda parte

--- js/script.js
+++ js/script.js
@@ -1,15 +1,44 @@
+// faccio 16 numeri che vanno da 1  a  100
 var array = [];
-var random = Randomnum(1,100);
 
 while(array.length < 16){
 
     var random = randomnum(1,100);
 
     if(!array.includes(random)){
-        Array.push(random);
+        array.push(random);
     }
 }
 
-function Randomnum(min, max){
+function randomnum(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+console.log(array);
+
+// chiedo un numero da 1  a 100 e controllo che non sia ripetuto
+
+var numeroutente = parseInt(prompt('inserisci numero'));
+
+// if(array.includes(numeroutente)== true){
+
+//    console.log('hai perso')
+
+// }else{
+//     console.log('hai vinto')
+// }
+
+
+// funzione per regolare che il numero sia compreso tra 1 e 100
+function checkRange(min,max,valore){
+    return valore >= min && valore <= max;
+}
+
+var Min = 1;
+var Max = 100;
+var Bombe = 16;
+
+while(array.length < Max - Bombe && !fine){
+
+}if()
+
